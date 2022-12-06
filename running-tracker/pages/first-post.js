56fb3ce2214e93185e854ef3a365d8a78660852a
Lukasgrat <60186475+Lukasgrat@users.noreply.github.com>
2022-12-06 13:59:49 -0500

    Trying to edit css, text color is not recognized

--- running-tracker/pages/first-post.js
+++ running-tracker/pages/first-post.js
@@ -8,20 +8,40 @@ export default function FirstPost() {
       strategy="lazyOnload"
       onLoad={() =>
         {const button = document.createElement('button')
-
-        // Set the button text to 'Can you click me?'
         button.innerText = 'Can you click me?'
   
         button.id = 'mainButton'
-  
-        // Attach the "click" event to your button
+        const input = document.createElement("input");
+        input.setAttribute("id", "username");
+        input.setAttribute("type", "text");
+        document.body.appendChild(input);
+        const input2 = document.createElement("input");
+        input2.setAttribute("id", "username2");
+        input2.setAttribute("type", "text");
+        document.body.appendChild(input2);
+        const input3 = document.createElement("input");
+        input3.setAttribute("id", "username3");
+        input3.setAttribute("type", "text");
+        document.body.appendChild(input3);
+        /*
+        const label = document.createElement("label");
+        label.innerHTML = "Username: ";
+        var usernameText = document.getElementById("test");
+        label.setAttribute("for", "username");
+        label.setAttribute("id","test");
+
+        document.body.insertBefore(label, usernameText);*/
+        var x = "";
+        var y = "";
+        var z = "";
         button.addEventListener('click', () => {
-          // When there is a "click"
-          // it shows an alert in the browser
-          alert('Oh, you clicked me!')
-        })
-  
-        document.body.appendChild(button)}
+            x  = document.getElementById("username").value;
+            y = document.getElementById("username2").value;
+            z = document.getElementById("username3").value;
+            alert(x + y + z);
+          })
+        document.body.appendChild(button)
+    }
       }
       
     />
