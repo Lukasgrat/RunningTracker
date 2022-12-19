b0de736e4f2d2bf45c83fa2f431c8d04d1f2dee9
elewis804 <erok84506@gmai.com>
2022-12-19 14:16:09 -0500

    Started working on the actual profile page
    Created the files for the profile page and started setting up some of the requesting/responding for the profile page.

---
+++ running-tracker/pages/api/profile/[id].js
@@ -0,0 +1,24 @@
+const mysql = require('mysql2/promise');
+
+let connection;
+
+
+async function setUpConnection() {
+    connection = await mysql.createConnection({
+        host: 'soccerdb.calingaiy4id.us-east-2.rds.amazonaws.com',
+        user: 'lew_sch_teg',
+        password: '8nXUKMHfVUtY',
+        database: 'capstone_2223_5krun'
+    });
+}
+
+
+export default async function handler(req, res) {
+    const method = { req };
+
+    if (method === "GET") {
+        const { id } = req.query;
+        // TODO: Query DB to get information that matches the ID
+        return response.status(200).json(rows); // Incomplete; still need to get the running data, but this should get the basics
+    }
+}
