717768fa08745de533bfe62d0cda30fa7cf0a918
Lukasgrat <60186475+Lukasgrat@users.noreply.github.com>
2023-01-08 18:00:44 -0500

    Got auth0 on index navbar working

--- running-tracker/pages/userComponenets.js
+++ running-tracker/pages/userComponenets.js
@@ -1,15 +1,16 @@
-import {useUser } from '@auth0/nextjs-auth0/client';
+import {useUser} from '@auth0/nextjs-auth0/client';
 import React,{ Component } from 'react';
 import styles from '../styles/Home.module.css';
   const  Username = () => {
-    const{user, error, isLoading } = useUser();
-    if (isLoading) {
-      return (<div>Loading...</div>  );}
-    if (error){
-      return (<div>{error.message}</div>);}
-    if (user) {
-        return (<div href="/api/auth/logout" className={styles.NavTextRight}>Logout</div>);
+    const{user, error, isLoading} = useUser();
+    if(isLoading){
+      return (<a className={styles.NavTextRight} id ="login">Loading...</a>);
     }
-    return (<div href="/api/auth/login" className={styles.NavTextRight}>Login</div>);
+    if (error){
+      return (<div id ="login" >{error.message}</div>);}
+    if (user) {
+        return (<a href="/api/auth/logout" id = "login" className={styles.NavTextRight}>Logout</a>);
+    }
+    return (<a href="/api/auth/login" id ="login" className={styles.NavTextRight}>Login</a>);
     };
   export default Username;
