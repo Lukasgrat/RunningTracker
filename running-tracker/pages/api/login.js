69f4e6452976765d102624b371c9642bf3bd7f9c
elewis804 <erok84506@gmai.com>
2023-01-24 14:00:42 -0500

    Refactored connecting the database and created a dynamic route for /profile

---
+++ running-tracker/pages/api/login.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2');
+const db = require('../../db/db_connection.js')
+
+export default async function handler(req, res) {
+    let { method } = req;
+    if (method === 'POST') {
+        // TODO: Check if the person's email is already in the database. If it is not, add them to the database.
+    }
+}
--- running-tracker/pages/api/races.js
+++ running-tracker/pages/api/races.js
@@ -1,23 +1,11 @@
 const mysql = require('mysql2/promise');
-
-let connection;
-
-
-async function setUpConnection() {
-    connection = await mysql.createConnection({
-        host: 'soccerdb.calingaiy4id.us-east-2.rds.amazonaws.com',
-        user: 'lew_sch_teg',
-        password: '8nXUKMHfVUtY',
-        database: 'capstone_2223_5krun'
-    });
-}
+const db = require('../../db/db_connection.js')
 
 export default async function handler(req, res) {
     const { method } = req;
-    await setUpConnection();
 
     if (method === "GET") {
-        const [rows, fields] = await connection.execute('SELECT * FROM `Race`');
+        const [rows, fields] = await db.execute('SELECT * FROM `Race`');
         console.log(rows);
         return res.status(200).json(rows);
     }
