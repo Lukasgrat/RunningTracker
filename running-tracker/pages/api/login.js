0c9cb039e806f466ddb481a7c0ab65c61838e3be
elewis804 <erok84506@gmai.com>
2023-01-30 13:36:48 -0500

    Finishing up merge apparently

--- running-tracker/pages/api/login.js
+++ running-tracker/pages/api/login.js
@@ -3,14 +3,8 @@ const db = require('../../db/db_connection.js')
 
 export default async function handler(req, res) {
     let { method } = req;
-<<<<<<< HEAD
-    let body = { req };
-=======
     let { body } =  req ;
     console.log(body);
-    
-    
->>>>>>> eff68dac89495477c1d84f01ecfb2a9035c11cdb
 
     if (method === 'POST') {
         // TODO: Check if the person's email is already in the database. If it is not, add them to the database.
