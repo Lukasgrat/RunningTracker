940f847bf89863c4b921310fc797473fd9370b94
elewis804 <erok84506@gmai.com>
2023-01-31 14:39:44 -0500

    Changed db_connection to use the regular mysql2 syntax instead of the promise syntax

--- running-tracker/pages/api/login.js
+++ running-tracker/pages/api/login.js
@@ -1,17 +1,20 @@
-const mysql = require('mysql2');
 const db = require('../../db/db_connection')
 
 export default async function handler(req, res) {
     let { method } = req;
 
+    if (db !== null) {
+        console.log("DB is not null");
+    }
+
     if (method === 'POST') {
         let { body } =  req;
-        db.execute("SELECT * FROM PERSON;", (error, results) => {
+        db.execute('SELECT * FROM `Person`', (error, results) => {
             if (error) {
-                res.status(500).send(error);
-            } else {
+                res.status(500);
+            } else if (results) {
                 res.status(200).json(results);
             }
-        })
+        });
     }
 }
--- running-tracker/pages/api/races.js
+++ running-tracker/pages/api/races.js
@@ -1,12 +1,21 @@
-const mysql = require('mysql2/promise');
 const db = require('../../db/db_connection.js')
 
 export default async function handler(req, res) {
     const { method } = req;
 
     if (method === "GET") {
+        /*
         const [rows, fields] = await db.execute('SELECT * FROM `Race`');
         console.log(rows);
-        return res.status(200).json(rows);
+        return res.status(200).json(rows);//*/
+
+        db.execute('SELECT * FROM `Race`;', (error, results) => {
+            if (error) {
+                res.status(500);
+            } else if (results) {
+                console.log(results);
+                res.status(200).json(200);
+            }
+        })
     }
 }
